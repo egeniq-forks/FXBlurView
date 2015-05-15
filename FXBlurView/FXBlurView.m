86099e23e6d779f8996dd2fef8dd8057003e6b8a
Johan Kool <johan@koolistov.net>
2015-05-15 11:01:26 +0800

    Dark blend mode

--- FXBlurView/FXBlurView.m
+++ FXBlurView/FXBlurView.m
@@ -100,7 +100,7 @@
     if (tintColor && CGColorGetAlpha(tintColor.CGColor) > 0.0f)
     {
         CGContextSetFillColorWithColor(ctx, [tintColor colorWithAlphaComponent:0.25].CGColor);
-        CGContextSetBlendMode(ctx, kCGBlendModePlusLighter);
+        CGContextSetBlendMode(ctx, kCGBlendModePlusDarker);
         CGContextFillRect(ctx, CGRectMake(0, 0, buffer1.width, buffer1.height));
     }
     
